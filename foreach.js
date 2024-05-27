956010cf281759fdd4e475703c10dfb838e6e34d
THIAGO <thiagohenssuchi@gmail.com>
2024-05-27 11:01:52 -0400

    treino 5.0

--- foreach.js
+++ foreach.js
@@ -1,20 +1,24 @@
-//ForEach é uma ação que permite executar uma função para cada elemento, sejam eles arrays simples ou de objetos. 
+/*ForEach é uma ação que permite executar uma função para cada elemento, sejam eles arrays simples ou de objetos. Diferente do FOR o Foreach precisa de um array inicial pois ele é um método que se executa apartir de um parametro existente. O FOREACH É UM MÉTODO SÍNCRONO!!!!!! */
 
 const notas = [10, 3, 44, 4.5, 10];
 
 let somaDasNotas = 0
 
-// notas.forEach(function (nota) {
-//     somaDasNotas += nota;
-// })
+notas.forEach(function (nota) {
+    somaDasNotas += nota;
+})
+
+nome.forEach((item, index, array) => {// ESSA É A ORDEM E OS PARAMETROS DE UM FOREACH 
+   console.log(index, item, array);
+})
 
 // ------------------ OU NA FORMA ARROW-FUNCTION ------------------ //
 
-notas.forEach((nota) => somaDasNotas += nota);
+// notas.forEach((nota) => somaDasNotas += nota);
 
-const media = somaDasNotas / notas.length;
+// const media = somaDasNotas / notas.length;
 
-console.log(`A média das notas é ${media.toFixed(0)}`);
+// console.log(`A média das notas é ${media.toFixed(0)}`);
 
 // -----------------------------------------------------------------//
 /* O método forEach() recebe uma função como parâmetro, e esta função por sua vez utiliza como parâmetro cada um dos elementos do array. A lógica interna do forEach() vai manejar a iteração do primeiro ao último item do array e fazer com que o código definido dentro da função callback (no caso, apenas um incremento 'soma += numero') seja executado a cada iteração. 
---
+++ foreachexe2.js
@@ -0,0 +1,9 @@
+// const nums = [2,3,4,5,6,7];
+
+// for(let index = 0; index < nums.length; index++) {
+//     if (nums[index] === 5){
+//       break
+//     }
+//     console.log(nums[index]);
+// }
+
